0fe5a7a3793ee8c7b025d342acada479a907ec6e
Victor <vlenceleuth@gmail.com>
2025-09-25 21:54:21 +0800

    Make Arena an interface

--- arena.go
+++ arena.go
@@ -1,21 +1,30 @@
 package objpool
 
-// A simple arena allocator.
-type Arena []byte
+type Arena interface {
+	// Alloc allocates size bytes in the arena and returns a pointer to it.
+	// If size bytes aren't available then an empty slice is returned.
+	Alloc(size int) []byte
 
-// NewArena creates a new arena allocator of size cap.
-func NewArena(cap int) Arena {
+	// Free frees this arena and returns the number of bytes freed.
+	Free() int
+}
+
+// An arena allocator that zeroes the memory before returning it.
+type ZeroedArena []byte
+
+// NewZeroedArena creates a new arena allocator of size cap.
+func NewZeroedArena(cap int) ZeroedArena {
 	if cap < 0 {
-		return make(Arena, 0)
+		return make(ZeroedArena, 0)
 	}
 
-	return make(Arena, 0, cap)
+	return make(ZeroedArena, 0, cap)
 }
 
 // Alloc allocates size bytes in arena and returns them.
 // If size bytes isn't available then an empty slice is
 // returned. The bytes returned are zeroed.
-func (arena *Arena) Alloc(size int) []byte {
+func (arena *ZeroedArena) Alloc(size int) []byte {
 	i := len(*arena)
 	c := cap(*arena)
 	free := c-i
@@ -31,8 +40,7 @@ func (arena *Arena) Alloc(size int) []byte {
 	return (*arena)[i:i+size]
 }
 
-// Free frees up the arena's memory for future allocations.
-func (arena *Arena) Free() int {
+func (arena *ZeroedArena) Free() int {
 	used := len(*arena)
 
 	*arena = (*arena)[0:0]
